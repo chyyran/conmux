3cc68705d655cdd2d41f89b58be343d84444b952
Ronny Chan <ronny6993@gmail.com>
2018-11-20 17:35:42 -0500

    wincon: add functions to get the raw OS console handle

--- src/wincon.rs
+++ src/wincon.rs
@@ -1,19 +1,31 @@
 use winapi::um::consoleapi::{GetConsoleMode, SetConsoleMode};
 use winapi::um::wincon::{ENABLE_VIRTUAL_TERMINAL_PROCESSING, DISABLE_NEWLINE_AUTO_RETURN};
-use winapi::um::winbase::STD_OUTPUT_HANDLE;
-use winapi::um::winnt::HANDLE;
+use winapi::um::processenv::GetStdHandle;
+use winapi::um::winbase::{STD_OUTPUT_HANDLE, STD_INPUT_HANDLE};
 use winapi::shared::minwindef::DWORD;
 
-use std::ptr::null_mut;
+use std::io::{Write, Read};
+
+use std::os::windows::io::FromRawHandle;
+use std::fs::File;
 
 pub fn enable_console() {
 
-    let console_handle = unsafe { winapi::um::processenv::GetStdHandle(STD_OUTPUT_HANDLE) };
+    let console_handle = unsafe { GetStdHandle(STD_OUTPUT_HANDLE) };
     let mut console_mode: DWORD = 0;
-    let result = unsafe { winapi::um::consoleapi::GetConsoleMode(console_handle, &mut console_mode) };
+    let _result = unsafe { GetConsoleMode(console_handle, &mut console_mode) };
 
     console_mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING | DISABLE_NEWLINE_AUTO_RETURN;
-    let result = unsafe { SetConsoleMode(console_handle, console_mode) };
+    let _result = unsafe { SetConsoleMode(console_handle, console_mode) };
+}
+
+pub fn os_stdout() -> impl Write {
+    let console_handle = unsafe { winapi::um::processenv::GetStdHandle(STD_OUTPUT_HANDLE) };
+    unsafe { File::from_raw_handle(console_handle) } 
 }
 
 
+pub fn os_stdin() -> impl Read {
+    let console_handle = unsafe { winapi::um::processenv::GetStdHandle(STD_INPUT_HANDLE) };
+    unsafe { File::from_raw_handle(console_handle) } 
+}
